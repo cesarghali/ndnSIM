a4d483f66880010017dc2768362728c22323ef87
Cesar Ghali <cghali@uci.edu>
2013-06-26 19:01:25 -0700

    Minor change, remove extra \

--- model/ndn-content-object.cc
+++ model/ndn-content-object.cc
@@ -1,4 +1,4 @@
-\/* -*- Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil -*- */
+/* -*- Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2011-2013 University of California, Los Angeles
  *
